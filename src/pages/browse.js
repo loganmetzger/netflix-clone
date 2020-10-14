6f72a8d21e3af61a710a267e2c8328525e6d610c
Logan Metzger <loganmetzger@MacBook-Pro.local>
2020-10-14 16:22:54 -0400

    feature and header complete for home page

--- src/pages/browse.js
+++ src/pages/browse.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function Browse() {
-    return <></>
+    return <>Browse</>
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import { Feature, OptForm } from "../components";
+import { HeaderContainer } from "../containers/header";
 import { FaqsContainer } from "../containers/faqs";
 import { FooterContainer } from "../containers/footer";
 import { JumbotronContainer } from "../containers/jumbotron";
@@ -7,6 +9,25 @@ import { JumbotronContainer } from "../containers/jumbotron";
 export default function Home() {
   return (
     <>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>Unlimited films, TV, and more.</Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+
+          <OptForm>
+            <OptForm.Input placeholder="Email Address" />
+            <OptForm.Button>Try It Now!</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready To Watch? Enter Your Email to Creat or Restart Your
+              Membership
+            </OptForm.Text>
+          </OptForm>
+          
+        </Feature>
+      </HeaderContainer>
       <JumbotronContainer />
       <FaqsContainer />
       <FooterContainer />
